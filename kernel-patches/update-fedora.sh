e16d2ba2bb47fbb8732f2153609ff9582a24d9df
Gregregorovich <evelynngregoria@gmail.com>
2025-07-31 22:03:03 +0100

    Added a check to inform the user if they are on the latest kernel release

--- kernel-patches/update-fedora.sh
+++ kernel-patches/update-fedora.sh
@@ -147,7 +147,9 @@ echo
 read -p "Do you want to update all other packages and flatpaks on this system? (y/n) " answer
 if [[ $answer == y ]] || [[ $answer == yes ]] || [[ $answer == Y ]] || [[ $answer == YES ]] || [[ $answer == Yes ]]; then
     echo "Updating remaining packages and flatpaks..."
-    dnf update --refresh -y --exclude=kernel,kernel-core,kernel-modules,kernel-modules-core,kernel-modules-extra,kernel-devel,kernel-devel-matched,kernel-tools,kernel-tools-libs,kernel-tools-libs-devel
+    dnf update --refresh -y --exclude=kernel,kernel-core,kernel-modules,kernel-modules-core,kernel-modules-extra,kernel-modules-extra-matched,kernel-devel,kernel-devel-matched,kernel-tools,kernel-tools-libs,kernel-tools-libs-devel
     flatpak update -y
     echo "Updated remaining packages and flatpaks"
+    echo "Confirming running most recent kernel release:"
+    dnf check-update
 fi
